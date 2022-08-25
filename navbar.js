278b2ef1a7cc45b67bc66e9cdce85503105d7bae
kshit <chauhanparikshit7@gmail.com>
2022-08-26 01:50:47 +0530

    navbar

---
+++ navbar.js
@@ -0,0 +1,11 @@
+let clickBar=document.querySelector(".fa-bars");
+let Drop=ducument.querySelector(".drop-Down");
+
+clickBar.addEventListener("click", function (){
+    console.log(clickBar);
+
+    clickBar.classList.toggle("rotate");
+    Drop.classList.toggle("action");
+})
+
+console.log(kshit)
